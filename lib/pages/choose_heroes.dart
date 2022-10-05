94e4b5898b9d82597c7c20a2d39d52c4bb13fb5b
Julia1284 <julia123456-84@mail.ru>
2022-10-05 22:32:20 +0300

    navigation

---
+++ lib/pages/choose_heroes.dart
@@ -0,0 +1,30 @@
+import 'package:danek/main.dart';
+import 'package:flutter/material.dart';
+
+class ChooseHeroes extends StatelessWidget {
+  const ChooseHeroes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(''),
+          centerTitle: true,
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              Text('Выбрать героя'),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/gamepage');
+                  },
+                  child: Text('Далее')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/game_page.dart
@@ -0,0 +1,31 @@
+import 'package:danek/main.dart';
+import 'package:flutter/material.dart';
+
+class GamePage extends StatelessWidget {
+  const GamePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          child: Column(
+            children: [
+              Text('Игра'),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/');
+                  },
+                  child: Text('Меню')),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/shoppage');
+                  },
+                  child: Text('Магазин')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
