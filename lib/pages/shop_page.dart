1ab9ff6b4d474fddf2499f74739d5f5342774b13
Melamorri <yuliapavlovich@gmail.com>
2022-10-06 16:02:44 +0300

    some localization stuff

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,5 +1,6 @@
 import 'package:danek/main.dart';
 import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 class ShopPage extends StatelessWidget {
   const ShopPage({super.key});
