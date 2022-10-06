d12815f4e078f5eeb007c1e273a30a13ce059a3b
Melamorri <yuliapavlovich@gmail.com>
2022-10-06 16:03:52 +0300

    some localization stuff

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:danek/pages/menu_page.dart';
 import 'package:danek/pages/setting_page.dart';
 import 'package:danek/pages/shop_page.dart';
 import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 void main() => runApp(const MyApp());
 
