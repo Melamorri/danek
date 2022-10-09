94eafd56a644dbb31938641cc14cf568931be6dc
Melamorri <yuliapavlovich@gmail.com>
2022-10-10 01:42:14 +0300

    lclztn 3

---
+++ lib/generated/locale_keys.g.dart
@@ -0,0 +1,9 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const shop = 'shop';
+  static const play = 'play';
+  static const settings = 'settings';
+  static const exit = 'exit';
+
+}
