0c6c7659fe667a3c665c80f064e56d9d5f01327d
Melamorri <yuliapavlovich@gmail.com>
2022-10-10 01:17:29 +0300

    lclztn 2.0

--- lib/main.dart
+++ lib/main.dart
@@ -6,23 +6,25 @@ import 'package:danek/pages/setting_page.dart';
 import 'package:danek/pages/shop_page.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+// import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
 
-  runApp(EasyLocalization(
-      supportedLocales: [
-        Locale('en'),
-        Locale('kk'),
-        Locale('ky'),
-        Locale('ru'),
-        Locale('tg')
-      ],
-      path: 'assets/translations',
-      fallbackLocale: Locale('en'),
-      child: const MyApp()));
+  runApp(
+    EasyLocalization(
+        supportedLocales: const [
+          Locale('en'),
+          Locale('kk'),
+          Locale('ky'),
+          Locale('ru'),
+          Locale('tg')
+        ],
+        path: 'assets/translations',
+        fallbackLocale: const Locale('en'),
+        child: const MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
