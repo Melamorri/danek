5aeced309396c347d698c268c9db613e4662ea1e
murzik06.83 <murzik06.83@mail.ru>
2022-10-10 21:58:03 +0300

    shop_page

---
+++ lib/models/shop_models.dart
@@ -0,0 +1,134 @@
+class Shop {
+  String name;
+  String image;
+  String price;
+
+  Shop({
+    required this.name,
+    required this.image,
+    required this.price,
+  });
+}
+
+List<Shop> shopList = [
+  Shop(
+    name: 'Шляпа',
+    image: 'assets/images/hat.png',
+    price: '10 монет',
+  ),
+  Shop(
+    name: 'Колпак',
+    image: 'assets/images/kolpak.png',
+    price: '40 монет',
+  ),
+  Shop(
+    name: 'Майка',
+    image: 'assets/images/hat.png',
+    price: '5 монет',
+  ),
+  Shop(
+    name: 'Юбка',
+    image: 'assets/images/ubka.png',
+    price: '10 монет',
+  ),
+  Shop(
+    name: 'Юбка',
+    image: 'assets/images/ubka_2.png',
+    price: '8 монет',
+  ),
+  Shop(
+    name: 'Шляпа',
+    image: 'assets/images/hat.png',
+    price: '10 монет',
+  ),
+  Shop(
+    name: 'Шляпа',
+    image: 'assets/images/hat.png',
+    price: '10 монет',
+  ),
+  Shop(
+    name: 'Шляпа',
+    image: 'assets/images/hat.png',
+    price: '10 монет',
+  ),
+  Shop(
+    name: 'Шляпа',
+    image: 'assets/images/hat.png',
+    price: '10 монет',
+  ),
+  Shop(
+    name: 'Шляпа',
+    image: 'assets/images/hat.png',
+    price: '10 монет',
+  ),
+  Shop(
+    name: 'Шляпа',
+    image: 'assets/images/hat.png',
+    price: '10 монет',
+  ),
+  Shop(
+    name: 'Шляпа',
+    image: 'assets/images/hat.png',
+    price: '10 монет',
+  ),
+  Shop(
+    name: 'Шляпа',
+    image: 'assets/images/hat.png',
+    price: '10 монет',
+  ),
+  Shop(
+    name: 'Колпак',
+    image: 'assets/images/hat.png',
+    price: '40 монет',
+  ),
+  Shop(
+    name: 'Майка',
+    image: 'assets/images/hat.png',
+    price: '5 монет',
+  ),
+  Shop(
+    name: 'Юбка',
+    image: 'assets/images/hat.png',
+    price: '10 монет',
+  ),
+  Shop(
+    name: 'Шляпа',
+    image: 'assets/images/hat.png',
+    price: '8 монет',
+  ),
+  Shop(
+    name: 'Шляпа',
+    image: 'assets/images/hat.png',
+    price: '10 монет',
+  ),
+  Shop(
+    name: 'Шляпа',
+    image: 'assets/images/hat.png',
+    price: '10 монет',
+  ),
+  Shop(
+    name: 'Шляпа',
+    image: 'assets/images/hat.png',
+    price: '10 монет',
+  ),
+  Shop(
+    name: 'Шляпа',
+    image: 'assets/images/hat.png',
+    price: '10 монет',
+  ),
+  Shop(
+    name: 'Шляпа',
+    image: 'assets/images/hat.png',
+    price: '10 монет',
+  ),
+  Shop(
+    name: 'Шляпа',
+    image: 'assets/images/hat.png',
+    price: '10 монет',
+  ),
+  Shop(
+    name: 'Шляпа',
+    image: 'assets/images/hat.png',
+    price: '10 монет',
+  ),
+];
