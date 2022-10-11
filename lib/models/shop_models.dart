a66bb5e665097381bf94b1985818578ba5462826
murzik06.83 <murzik06.83@mail.ru>
2022-10-12 01:07:05 +0300

    shop_page

--- lib/models/shop_models.dart
+++ lib/models/shop_models.dart
@@ -1,12 +1,8 @@
-/// The [dart:async] is neccessary for using streams
 import 'dart:async';
 
 class Shop {
-  /// The [cartStreamController] is an object of the StreamController class
-  /// .broadcast enables the stream to be read in multiple screens of our app
   final cartStreamController = StreamController.broadcast();
 
-  /// The [getStream] getter would be used to expose our stream to other classes
   Stream get getStream => cartStreamController.stream;
 
   final Map shopList = {
@@ -112,22 +108,8 @@ class Shop {
   };
 
   void addToCart(item) {
-    //shopList['shop items'].remove(item);
     shopList['my_items'].add(item);
-    //cartStreamController.sink.add(shopList);
   }
-
-  // void removeFromCart(item) {
-  //   shopList['cart items'].remove(item);
-  //   shopList['shop items'].add(item);
-  //   cartStreamController.sink.add(shopList);
-  // }
-
-  /// The [dispose] method is used
-  /// to automatically close the stream when the widget is removed from the widget tree
-  // void dispose() {
-  //   cartStreamController.close(); // close our StreamController
-  // }
 }
 
 final bloc = Shop();
