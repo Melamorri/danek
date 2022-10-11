559496029dfbcbd8d9c1651bfe59e5b64be506a9
Natalia <977605@mail.ru>
2022-10-11 19:22:37 +0300

    добавлена страница с барсом, с нее реализован переход к активностям: чистить зубы, читать книгу, играть в игрушки, кушать, делать зарядку, на все страницы активностей добавлен таймер

---
+++ lib/pages/hero_page_bars.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'page_for_activity.dart';
+import 'package:danek/models/activity_list.dart';
+
+class HeroList extends StatefulWidget {
+  const HeroList({Key? key}) : super(key: key);
+
+  @override
+  HeroListState createState() => HeroListState();
+}
+
+class HeroListState extends State<HeroList> {
+  int _selectedIndex = -1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/bars.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            _addSpace(550),
+            _addHorizontalList(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _addHorizontalList() {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.15,
+      child: ListView.builder(
+          shrinkWrap: true,
+          scrollDirection: Axis.horizontal,
+          padding: const EdgeInsets.all(20),
+          itemCount: ativityList.length,
+          itemBuilder: (BuildContext context, int index) {
+            ActivityList activity = ativityList[index];
+            return Container(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              width: MediaQuery.of(context).size.width * 0.3,
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ActivityDetailsScreen(ativityList: activity)));
+                },
+                child: ListTile(
+                  contentPadding: const EdgeInsets.all(1),
+                  leading:
+                  Container(
+                    width: 70,
+                    child: CircleAvatar(
+                      radius: 70.0,
+                      backgroundImage: AssetImage(activity.image),
+                    ),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: _selectedIndex == index
+                            ? Colors.blue
+                            : Colors.black12,
+                        width: 3.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
+
+Widget _addSpace(double space) {
+  return SizedBox(
+    height: space,
+  );
+}
