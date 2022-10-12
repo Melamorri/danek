2309d849ff66f218ba0beee467ddfbb10f801130
Melamorri <yuliapavlovich@gmail.com>
2022-10-12 13:10:16 +0300

    lclz

--- lib/generated/locale_keys.g.dart
+++ lib/generated/locale_keys.g.dart
@@ -1,10 +1,16 @@
 // DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
 
-abstract class  LocaleKeys {
+abstract class LocaleKeys {
   static const shop = 'shop';
   static const play = 'play';
   static const settings = 'settings';
   static const exit = 'exit';
   static const menu = 'menu';
-
+  static const language = 'language';
+  static const swipe = 'swipe';
+  static const select_character = 'select_character';
+  static const enter_name = 'enter_name';
+  static const name = 'name';
+  static const age = 'age';
+  static const enter_age = 'enter_age';
 }
