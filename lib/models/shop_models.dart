2df9b6eafd2300def1bf857b0380f43f784ba74e
murzik06.83 <murzik06.83@mail.ru>
2022-10-13 22:35:57 +0300

    zzz

--- lib/models/shop_models.dart
+++ lib/models/shop_models.dart
@@ -97,6 +97,24 @@ class Shop {
         'price': '20',
         'id': 5
       },
+      {
+        'name': 'Майка',
+        'image': 'assets/images/t-shirt.png',
+        'price': '10',
+        'id': 3
+      },
+      {
+        'name': 'Шляпа',
+        'image': 'assets/images/hat.png',
+        'price': '90',
+        'id': 4
+      },
+      {
+        'name': 'Пальто',
+        'image': 'assets/images/coat.png',
+        'price': '20',
+        'id': 5
+      },
     ],
     'my_items': []
   };
