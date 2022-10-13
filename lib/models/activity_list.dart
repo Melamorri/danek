9765422760ad87a09ca814f6ad766bad1ca2e6bd
Natalia <977605@mail.ru>
2022-10-13 20:31:53 +0300

    - в проект добавлены gif - вызов картинки для активности из asset - добавлено свое время таймера на каждую активность

--- lib/models/activity_list.dart
+++ lib/models/activity_list.dart
@@ -6,44 +6,48 @@ class ActivityList {
   String image;
   String gif;
   String wav;
+  int id;
 
   ActivityList({
     required this.name,
     required this.image,
     required this.gif,
     this.wav = '',
+    required this.id,
   });
 }
 
 List<ActivityList> ativityList = [
   ActivityList(
+    id: 1,
     name: LocaleKeys.read_book.tr(),
     image: 'assets/images/readbookbutton.png',
-    gif:
-        'https://advance-club.ru/images/uploads/kakie-knigi-predlojit-rebenku-2.gif',
+    gif: 'assets/images/reeding_books.gif',
   ),
   ActivityList(
+      id: 2,
       name: LocaleKeys.teeth.tr(),
       image: 'assets/images/toothbrushbutton.png',
-      gif: 'http://file.mobilmusic.ru/7d/28/db/878164.gif',
+      gif: 'assets/images/frog.gif',
       wav: 'teeth.mp3'),
   ActivityList(
+      id: 3,
       name: LocaleKeys.toys.tr(),
       image: 'assets/images/toysbutton.png',
-      gif: 'https://i.gifer.com/aMs.gif',
+      gif: 'assets/images/fold_toys.gif',
       wav: 'toys.mp3'),
   ActivityList(
+    id: 4,
     name: LocaleKeys.eat.tr(),
     image: 'assets/images/eatbutton.png',
-    gif:
-        'https://i.pinimg.com/originals/26/6a/31/266a31e4d7dcf4eb9deba7dd32ca8a34.gif',
+    gif: 'assets/images/eating.gif',
   ),
   ActivityList(
+    id: 5,
     name: LocaleKeys.exercise.tr(),
     image: 'assets/images/gymbutton.png',
-    gif: 'https://media.tenor.com/fhBpSJxEozwAAAAC/iworkout-winniethepooh.gif',
+    gif: 'assets/images/excercise.gif',
   ),
 ];
 // нужно тогда добавить сюда же  стоимость действия,  это переменная cash
-//прописано в helpers/activity_button   
-
+//прописано в helpers/activity_button
--- lib/pages/page_for_activity.dart
+++ lib/pages/page_for_activity.dart
@@ -16,6 +16,30 @@ class ActivityDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final CountDownController controller = CountDownController();
+    List<int> timelist = [600, 120, 300, 900, 600];
+    late int time;
+
+    int _chekId() {
+      if (ativityList.id == 1) {
+        return timelist.elementAt(0);
+      }
+      if (ativityList.id == 2) {
+        return timelist.elementAt(1);
+      }
+      if (ativityList.id == 3) {
+        return timelist.elementAt(2);
+      }
+      if (ativityList.id == 4) {
+        return timelist.elementAt(3);
+      }
+      if (ativityList.id == 5) {
+        return timelist.elementAt(4);
+      }
+      throw 'Нет нужного элемента';
+    }
+
+    time = _chekId();
+
     return Scaffold(
       body: Container(
         width: double.infinity,
@@ -66,10 +90,10 @@ class ActivityDetailsScreen extends StatelessWidget {
                 ),
                 Text(ativityList.name.toString(),
                     style: Theme.of(context).textTheme.headline6),
-                Image(
-                  image: NetworkImage(ativityList.gif.toString()),
-                  width: 500,
-                  height: 350,
+                Image.asset(
+                  ativityList.gif.toString(),
+                  height: 325.0,
+                  width: 325.0,
                 ),
                 Padding(
                   padding: const EdgeInsets.all(30),
@@ -83,11 +107,11 @@ class ActivityDetailsScreen extends StatelessWidget {
                             }),
                         NeonCircularTimer(
                             onComplete: () {
-                              controller.restart();
+                              // controller.restart();
                             },
                             width: 80,
                             controller: controller,
-                            duration: 120,
+                            duration: time,
                             strokeWidth: 3,
                             textStyle: const TextStyle(fontSize: 20),
                             isTimerTextShown: true,
