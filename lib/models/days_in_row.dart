ae1bb4572ec6eddb4cf183d0cf623d979232eb30
OxySqualo <oxana.xena@gmail.com>
2022-10-20 01:37:25 +0300

    ok

--- lib/models/days_in_row.dart
+++ lib/models/days_in_row.dart
@@ -1,11 +1,8 @@
 import 'package:intl/intl.dart';
 
-// this gets the last counter from the database
-void getExtraCoinsForDays() {
-// this gets the last counter from the database
-
+void getExtraCoin() {
   int getLastCounter() {
-    return 2; // количество входов = 0 в начале игры. как оно будет обновляться?
+    return 2; //кол-во для теста
   }
 
 // this sets the database counter
@@ -16,57 +13,42 @@ void getExtraCoinsForDays() {
 // this gets the last date the user logged in from database
   DateTime loadLastTime() {
     return (DateTime.parse(
-        "2022-10-15")); //<-- alter this date to see code change
+        "2022-10-19")); //<-- alter this date to see code change
   }
 
 // This sets new login date
   void setNewTime(date) {
-    print('Set last login date to: $date');
+    print('Set last login date to: $date'); //
   }
 
+//функция возвращает сегодняшнюю дату в формате yyyy-MM-dd
   DateTime getJustDate(DateTime date) {
     final DateFormat formatter = DateFormat('yyyy-MM-dd');
     final formatted = formatter.format(date);
     return DateTime.parse(formatted);
   }
 
-  // run this on login event
   int counter = getLastCounter();
   // print('current login count: $counter');
   final today = getJustDate(DateTime.now());
   final last = getJustDate(loadLastTime());
-  if (today.difference(last) == Duration(hours: 24)) {
-    setCounter(counter + 1);
+  if (today.difference(last) == Duration(days: 1)) {
+    setCounter(counter++);
+    print(counter);
   } else {
     setCounter(0);
+    print(counter);
   }
   setNewTime(today); //  обновляется  дата входа
 
-  List<int> extralist = [100, 500, 1000, 1500, 2000];
-  late int extra;
-
-  int getExtraCoins() {
-    if (counter + 1 == 3) {
-      return extralist.elementAt(0);
-    } else if (counter + 1 == 5) {
-      return extralist.elementAt(1);
-    } else if (counter + 1 == 10) {
-      return extralist.elementAt(2);
-    } else if (counter == 15) {
-      return extralist.elementAt(3);
-    } else if (counter + 1 == 20) {
-      return extralist.elementAt(4);
-    }
-    throw 'Нет бонуса';
+  Map<int, int> extraMap = {3: 100, 5: 500, 10: 1000, 15: 1500, 20: 2000};
+  var keySet = extraMap.keys;
+  int myCoins = 0;
+  if (keySet.contains(counter)) {
+    myCoins = extraMap[counter]! + myCoins; //
+    print("бонусные монетки: ${extraMap[counter]}");
+    print("Всего: $myCoins");
+  } else {
+    throw "нет бонуса";
   }
-
-  extra = getExtraCoins();
-  int myCoins = 0; //не уверена насчет названия переменной, куда добавлять
-  myCoins = myCoins + extra; // обновление счета монеток
-  print("бонусные монетки: $extra");
-  print("общий счет: $myCoins");
-  print('количество дней в игре без перерыва: ${counter + 1}');
 }
-
-// куда прикрутить эту функцию?
-//
